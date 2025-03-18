4a3835de4ece6a6807b189a6b2f20e1611537afe
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-18 17:03:26 +0600

    silder some design

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Slider from '../components/Slider';
 
 const HomeLayout = () => {
     return (
-        <div className='lg:px-32 md:px-20 px-10'>
-            <h3 className="text-3xl">Hello</h3>
+        <div className='mt-3'>
+            <Slider/>
+            {/* other components  */}
+            <div className="lg:px-32 md:px-20 px-10">
 
+            </div>
         </div>
     );
 };
