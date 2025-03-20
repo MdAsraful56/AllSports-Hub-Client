668b01309090c2781b02b2b6cbc606d20104a40e
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-20 17:29:43 +0600

    2 slider is responsive

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,33 +9,61 @@ const Slider = () => {
     return (
         <div>
             <div className="carousel w-full">
-            <div id="item1" className="carousel-item w-full relative">
-                
-                </div>
-
-                <div id="item2" className="carousel-item w-full bg-black flex flex-row-reverse">
-                    <div className="pr-20">
-                        <img src={Slider2} className="w-full h-96 object-cover" />
+                <div id="item1" className="carousel-item w-full bg-black flex lg:flex-row-reverse flex-col-reverse  ">
+                    <div className="lg:w-1/2 w-full">
+                        <img src={Slider1} className="lg:w-full h-96 lg:object-cover object-contain" />
                     </div>
-                    <div className="text-white text-left flex flex-col justify-center space-y-4">
+                    <div className="text-white text-left flex flex-col justify-center space-y-4 rancho-font lg:w-1/2 w-full lg:px-10 px-4">
+                        <h2 className='text-2xl font-semibold'>ASICS Gel-Trabuco 11</h2>
+                        <ul>
+                            <li className=""> <span className="text-red-500 font-bold">Style:</span> All-Terrain Running Shoe</li>
+                            <li className=""> <span className="text-red-500 font-bold">Features:</span> GEL™ technology cushioning, DUOMAX™ support system, and ASICSGRIP™ outsole for enhanced durability and stability</li>
+                            <li className=""> <span className="text-red-500 font-bold">Colorway:</span> Midnight Blue, Electric Yellow, and Black</li>
+                        </ul>
+                        <button className="btn btn-info hover:text-white hover:border-white  hover:bg-gradient-to-r from-green-400  to-blue-500">Details</button>
+                    </div>
+                </div>
+                <div id="item2" className="carousel-item w-full bg-black flex lg:flex-row-reverse flex-col-reverse ">
+                    <div className="pr-20">
+                        <img src={Slider2} className="lg:w-full h-96 lg:object-cover object-contain" />
+                    </div>
+                    <div className="text-white text-left flex flex-col justify-center space-y-4 rancho-font lg:w-1/2 w-full lg:px-10 px-4">
                         <h2 className='text-2xl font-semibold'> Nike Velocity Blaze</h2>
                         <ul>
-                            <li className="">Style: Performance Training Shoe</li>
-                            <li className="">Features: High-energy return, durable outsole for traction</li>
-                            <li className="">Colorway: Fiery Gradient with Contrasting Cool-Tone Highlights</li>
+                            <li className=""> <span className="text-red-500 font-bold">Style:</span> Performance Training Shoe</li>
+                            <li className=""> <span className="text-red-500 font-bold">Features:</span> High-energy return, durable outsole for traction</li>
+                            <li className=""> <span className="text-red-500 font-bold">Colorway:</span> Fiery Gradient with Contrasting Cool-Tone Highlights</li>
                         </ul>
-                        <button className="btn">Details</button>
+                        <button className="btn btn-info hover:text-white hover:border-white  hover:bg-gradient-to-r from-green-400  to-blue-500">Details</button>
                     </div>
                 </div>
-                <div id="item3" className="carousel-item w-full">
-                    <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                    className="w-full" />
+                <div id="item3" className="carousel-item w-full bg-black flex lg:flex-row-reverse flex-col-reverse ">
+                    <div className="pr-20 w-1/2">
+                        <img src={Slider3} className="lg:w-full h-96 lg:object-cover object-contain" />
+                    </div>
+                    <div className="text-white text-left flex flex-col justify-center space-y-4 rancho-font lg:w-1/2 w-full lg:px-10 px-4">
+                        <h2 className='text-2xl font-semibold'> Nike Velocity Blaze</h2>
+                        <ul>
+                            <li className=""> <span className="text-red-500 font-bold">Style:</span> Performance Training Shoe</li>
+                            <li className=""> <span className="text-red-500 font-bold">Features:</span> High-energy return, durable outsole for traction</li>
+                            <li className=""> <span className="text-red-500 font-bold">Colorway:</span> Fiery Gradient with Contrasting Cool-Tone Highlights</li>
+                        </ul>
+                        <button className="btn btn-info hover:text-white hover:border-white  hover:bg-gradient-to-r from-green-400  to-blue-500">Details</button>
+                    </div>
                 </div>
-                <div id="item4" className="carousel-item w-full">
-                    <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                    className="w-full" />
+                <div id="item4" className="carousel-item w-full bg-black flex lg:flex-row-reverse flex-col-reverse ">
+                    <div className="pr-20 w-1/2 items-center">
+                        <img src={Slider4} className="lg:w-full h-96 lg:object-cover object-contain" />
+                    </div>
+                    <div className="text-white text-left flex flex-col justify-center space-y-4 rancho-font lg:w-1/2 w-full lg:px-10 px-4">
+                        <h2 className='text-2xl font-semibold'> Nike Velocity Blaze</h2>
+                        <ul>
+                            <li className=""> <span className="text-red-500 font-bold">Style:</span> Performance Training Shoe</li>
+                            <li className=""> <span className="text-red-500 font-bold">Features:</span> High-energy return, durable outsole for traction</li>
+                            <li className=""> <span className="text-red-500 font-bold">Colorway:</span> Fiery Gradient with Contrasting Cool-Tone Highlights</li>
+                        </ul>
+                        <button className="btn btn-info hover:text-white hover:border-white  hover:bg-gradient-to-r from-green-400  to-blue-500">Details</button>
+                    </div>
                 </div>
             </div>
             <div className="flex w-full justify-center gap-2 py-2">
