a871500efdd902e347661a2b867502353bd19076
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-24 14:09:09 +0600

    details page back link added

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
+import { FaArrowLeft } from "react-icons/fa";
 
 const Details = () => {
 
@@ -10,6 +11,9 @@ const Details = () => {
 
     return (
         <div className='m-20'>
+                <Link to='/allsportsequipment'>
+                    <button className="text-xl p-1 rounded-xl py-2 rancho-font gap-2 flex flex-row items-center hover:text-white hover:border-white  hover:bg-gradient-to-r from-green-400  to-blue-500"> <span><FaArrowLeft /></span> Back</button>
+                </Link>
             <div className="flex lg:flex-row flex-col items-center justify-center gap-10 rancho-font">
                 <div className="">
                     <img src={photo} className='w-full h-[290px] object-cover rounded-lg border-2 border-gray-300 shadow-md' alt="" />
